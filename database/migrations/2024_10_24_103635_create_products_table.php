84c86023e1a0910bd9fb3c2568f03066f1c51893
sife22 <sifeddinehadi22@gmail.com>
2024-10-24 11:49:25 +0100

    [+] add product: Controller, Migration: Model

---
+++ database/migrations/2024_10_24_103635_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('sku')->nullable()->unique();
+            $table->string('status')->nullable()->unique();
+            $table->text('variations')->nullable();
+            $table->decimal('price', 7, 2)->nullable();
+            $table->string('currency', 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
