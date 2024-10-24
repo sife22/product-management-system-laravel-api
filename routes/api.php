1327115f2682641339f61df60a369a63e01e8e85
sife22 <sifeddinehadi22@gmail.com>
2024-10-24 19:53:40 +0100

    [+] add api endpoint: /products/import

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/products/import', [ProductController::class, 'import']);
